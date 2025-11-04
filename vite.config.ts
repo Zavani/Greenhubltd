c699f7210b177388aa29d5bd2ce1967ae8e7164e
Razvan <razvan@Razvans-MacBook-Air.local>
2025-11-04 23:08:23 +0200

    Re-apply custom domain configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Greenhubltd",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
